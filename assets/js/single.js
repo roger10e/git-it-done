e527431cf1f54816775cd43dce5ba6125bdaf8d2
roger10e <rogertenny@gmail.com>
2022-03-30 13:33:48 -0500

    finished 6.3

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,18 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
+var displayWarning = function(repo) {
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl =  "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -11,6 +24,10 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data) {
                 //pass response data to DOM funciton
                 displayIssues(data);
+                //check if API has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert("There was a problem with your request!")
